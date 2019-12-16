a98f3af6b904a96093011fc10c7700829c954854
ljt4418 <luke.thorpe18@imperial.ac.uk>
2019-12-16 03:51:59 +0000

    Implemented size method

--- src/pulchritudinous/structures/LinkedList.java
+++ src/pulchritudinous/structures/LinkedList.java
@@ -4,4 +4,8 @@ public class LinkedList {
   public boolean isEmpty() {
     return true;
   }
+
+  public int size() {
+    return 0;
+  }
 }
--- test/pulchritudinous/structures/LinkedListTest.java
+++ test/pulchritudinous/structures/LinkedListTest.java
@@ -3,10 +3,17 @@ package pulchritudinous.structures;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class LinkedListTest {
   @Test
   public void isEmptyUponInitialization() {
     assertTrue(new LinkedList().isEmpty());
+  }
+
+  @Test
+  public void hasSizeOfZeroUponInitialization() {
+    assertThat(new LinkedList().size(), is(0));
   }
 }
