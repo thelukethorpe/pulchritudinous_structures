0c3c5ae80cc19dca8ad610bbcb7b506001b26b83
ljt4418 <luke.thorpe18@imperial.ac.uk>
2019-12-21 22:16:49 +0000

    Implemented put method

--- src/pulchritudinous/structures/TreeMap.java
+++ src/pulchritudinous/structures/TreeMap.java
@@ -1,11 +1,120 @@
 package pulchritudinous.structures;
 
-public class TreeMap {
+public class TreeMap<K extends Comparable<K>, V> {
+
+  private final Node root;
+  private int size;
+
+  public TreeMap() {
+    this.root = new LinkNode();
+    this.size = 0;
+  }
+
+  private Node findNodeByKey(K key) {
+    Node prev = root;
+    Node curr = root.next(key);
+
+    while (curr != null) {
+      prev = curr;
+      curr = curr.next(key);
+    }
+
+    return prev;
+  }
+
   public boolean isEmpty() {
-    return true;
+    return size == 0;
+  }
+
+  public V put(K key, V value) {
+    Node node = findNodeByKey(key);
+    return node.add(key, value);
   }
 
   public int size() {
     return 0;
+  }
+
+  private abstract class Node {
+    public abstract V add(K key, V value);
+
+    public abstract Node next(K key);
+
+    public abstract V replace(K key, V value);
+  }
+
+  private class InternalNode extends Node {
+    private final LinkNode left, right;
+    private final K key;
+    private V value;
+
+    private InternalNode(K key, V value) {
+      this.left = new LinkNode();
+      this.right = new LinkNode();
+      this.key = key;
+      this.value = value;
+    }
+
+    @Override
+    public V add(K key, V value) {
+      Node node;
+      int thisMinusThat = this.key.compareTo(key);
+      if (thisMinusThat > 0) {
+        node = left;
+      } else if (thisMinusThat < 0) {
+        node = right;
+      } else {
+        node = this;
+      }
+      return node.replace(key, value);
+    }
+
+    @Override
+    public Node next(K key) {
+      int thisMinusThat = this.key.compareTo(key);
+      if (thisMinusThat > 0) {
+        return this.left.next(key);
+      } else if (thisMinusThat < 0) {
+        return this.right.next(key);
+      }
+      return null;
+    }
+
+    @Override
+    public V replace(K key, V value) {
+      assert (this.key.compareTo(key) == 0);
+      V prev = this.value;
+      this.value = value;
+      return prev;
+    }
+  }
+
+  private class LinkNode extends Node {
+    private InternalNode child;
+
+    private LinkNode() {
+      this.child = null;
+    }
+
+    @Override
+    public V add(K key, V value) {
+      assert (this.child == null);
+      return this.replace(key, value);
+    }
+
+    @Override
+    public Node next(K UNUSED) {
+      return this.child;
+    }
+
+    @Override
+    public V replace(K key, V value) {
+      if (child == null) {
+        child = new InternalNode(key, value);
+        size++;
+        return null;
+      }
+      return child.replace(key, value);
+    }
   }
 }
--- test/pulchritudinous/structures/TreeMapTest.java
+++ test/pulchritudinous/structures/TreeMapTest.java
@@ -2,20 +2,41 @@ package pulchritudinous.structures;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
 
 public class TreeMapTest {
-  private final TreeMap treeMap = new TreeMap();
+  private final TreeMap<String, Integer> treeMap = new TreeMap<>();
 
   @Test
   public void isEmptyUponInitialization() {
-    assertTrue(new TreeMap().isEmpty());
+    assertTrue(new TreeMap<>().isEmpty());
   }
 
   @Test
   public void hasSizeOfZeroUponInitialization() {
-    assertThat(new TreeMap().size(), is(0));
+    assertThat(new TreeMap<>().size(), is(0));
+  }
+
+  @Test
+  public void isNotEmptyAfterMappingHasBeenAdded() {
+    treeMap.put("Something", 1);
+    assertFalse(treeMap.isEmpty());
+  }
+
+  @Test
+  public void remappingReturnsOldValue() {
+    int n = 5;
+    Integer[] squares = new Integer[]{null, 1, 4, 9, 16, 25};
+    Integer[] fibs = new Integer[]{null, 1, 1, 2, 3, 5};
+    Integer[] primes = new Integer[]{null, 2, 3, 5, 7, 11};
+
+    for (int i = 0; i < n; i++) {
+      assertThat(treeMap.put("Square", squares[i + 1]), is(squares[i]));
+      assertThat(treeMap.put("Fib", fibs[i + 1]), is(fibs[i]));
+      assertThat(treeMap.put("Prime", primes[i + 1]), is(primes[i]));
+    }
   }
 }
